51286a5e8c90ba056c026102291d5eb5ff6c9027
Sheldon Fernandes <fernsheldon@gmail.com>
2017-09-17 14:01:11 +0530

    added test post method to see if we get post messages from API.AI

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ var mysql = require('mysql');
 var parser = require('body-parser');
 var pg = require('pg');
 var app = express();
+
+
 app.use(parser.json());
 app.use(parser.urlencoded({
   extended: true
 }));
 
 
+
+/**
+ * Database connection
+ * TODO: remove the db credentials
+ * from here later on.
+ */
 var connection = mysql.createConnection({
   host: 'us-cdbr-iron-east-05.cleardb.net',
   user: 'bab6e201bd92f0',
@@ -24,10 +32,6 @@ function handle_database(req, res) {
     var query = "select investors from transactions where city like '" + req.body.data.city + "%'";
   }
 
-  //else{
-  //    var query = "select company from transactions where investors like '" + req.body.data.investor + "%'";
-  //}
-  //var query = "select year from transactions where name = ' + req.body.data.city + '";
   connection.query(query, function(err, rows, fields) {
     if (err) {
       res.json({
@@ -46,57 +50,34 @@ function handle_database(req, res) {
     });
   });
 
-  //    res.json({"code" : 200, "status" : "Success"});
-  //    return;
-  //
-  //    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  //        client.query('SELECT * FROM transactions', function(err, result) {
-  //        done();
-  //      if (err)
-  //          { console.error(err); response.send("Error " + err); }
-  //      else
-  //          { response.render('pages/getTransactions', {results: result.rows} ); }
-  //      });
-  //    });
-
-
-  //    pool.getConnection(function(err,connection){
-  //        if (err) {
-  //          res.json({"code" : 100, "status" : "Error in connection database"});
-  //          return;
-  //        }
-  //        console.log('connected as id ' + connection.threadId);
-  //        console.log("Request" + req.body.data.city);
-  //        var query = "select * test_users";
-  //      // var query = "select * from transactions where city='" + req.body.data.city + "'";
-  //        connection.query(query,function(err,rows){
-  //            connection.release();
-  //            if(!err) {
-  //				res.json({"code" : 200, "data": rows});
-  //				//res.json({"code" : 200, "status" : req.body.data.city});
-  //			}
-  //        });
-  //        connection.on('error', function(err) {
-  //              res.json({"code" : 100, "status" : "Error in connection database"});
-  //              return;
-  //        });
-  //  });
 }
 
 
-app.get('/', function(request, response) {
-  connection.query("select * from transactions where city = 'Mumbai'", function(err, rows, fields) {
+/**
+ * This is the default 'get' method that gets
+ * called when the heroku app gets loaded.
+ * This can be used to see to if the app
+ * is in running condition or not.
+ */
+app.get('/', function(req, res) {
+  connection.query("select count(*) from transactions", function(err, rows, fields) {
     if (err) {
       console.log('error: ', err);
       throw err;
     }
-    response.send(['Running test query: ', rows]);
+    res.send(['Running test query, total rows of data available: ', rows]);
   });
 });
 
 app.post("/getTransactions", function(req, res) {
   handle_database(req, res);
 });
+
+
+app.post("/testPost", function(req, res) {
+  res.send(['Got the following request from API.AI: ', req]);
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
