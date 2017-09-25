5354feb893bac462774e3d8cb1dc5c0dc902dbd9
Surreal-sarvesh <30625354+Surreal-sarvesh@users.noreply.github.com>
2017-09-25 11:37:38 +0530

    adding comments
    sdjkfnsd

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function handle_database(req, res) {
   });
 
 }
-//gfghhjvhvj2324232sbdjhfbsdjfbsdhdsdfsdsfdfs
+//gfghhjvhvj2324232sbdjhfbsdjfbsdhdsdfsdsfdfssdfs
 
 /**
  * This is the default 'get' method that gets
