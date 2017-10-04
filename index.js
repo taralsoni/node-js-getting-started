421150602d6bacfb2270b577e9555601fd461116
Surreal-sarvesh <30625354+Surreal-sarvesh@users.noreply.github.com>
2017-10-04 17:53:13 +0530

    some mistake in query5
    sjkdfbs

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ var querytype = '0';
     var query = "select company from transactions where description like '%data%' and city like '" + req.body.data.city + "%'";
     querytype = '4';
   }
-  else if(req.body.data.investor == '0' && req.body.data.city =='0' && req.body.data.company == '0' && req.body.data.finflag == "0" && req.body.data.funding != '0') {
+  else if(req.body.data.investor == '0' && req.body.data.city =='0' && req.body.data.company != '0' && req.body.data.finflag == "0" && req.body.data.funding != '0') {
     // var query = "select company from transactions where city like '" + req.body.data.city + "%'";
      var query = "select total_funding from transactions where company like '" + req.body.data.company + "%'";
      querytype = '5';
