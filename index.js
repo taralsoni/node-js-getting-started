1d70e9cf9497d1c55eb307f8be5472f579421ed5
Surreal-sarvesh <30625354+Surreal-sarvesh@users.noreply.github.com>
2017-10-13 12:28:10 +0530

    app crashed again
    sfbsfkh

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(parser.urlencoded({
 /**
  * Database connection
  * TODO: remove the db credentials
- * from here later on.fhgfhgfghfgfghhsdjfhsdjsdhfsjfsdfasdadasdfsdfdsadjksdfhsdsdfdfjnkyusfdfs sdgdkfgsdsssdf
+ * from here later on.fhgfhgfghfgfghhsdjfhsdjsdhfsjfsdfasdadasdfsdfdsadjksdfhsdsdfdfjnkyusfdfs sdgdkfgsdssdf
  */
 var connection = mysql.createConnection({
   host: 'us-cdbr-iron-east-05.cleardb.net',
