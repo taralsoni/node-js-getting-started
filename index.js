1ed8126fd9cb649e2219990252cb4f6615ce274d
Surreal-sarvesh <30625354+Surreal-sarvesh@users.noreply.github.com>
2017-10-13 12:14:52 +0530

    aa
    aaa

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(parser.urlencoded({
 /**
  * Database connection
  * TODO: remove the db credentials
- * from here later on.fhgfhgfghfgfghhsdjfhsdjsdhfsjfsdfasdadasdfsdfdsadjksdfhsdsdfdfjnkyusdfgs  sdfsdFsdfdfs sdgdkfgsdsssdf
+ * from here later on.fhgfhgfghfgfghhsdjfhsdjsdhfsjfsdfasdadasdfsdfdsadjksdfhsdsdfdfjnkyusfdfs sdgdkfgsdsssdf
  */
 var connection = mysql.createConnection({
   host: 'us-cdbr-iron-east-05.cleardb.net',
