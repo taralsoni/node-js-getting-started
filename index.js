6efb483a28327b1926acd1f5b612581e5c48bc80
Surreal-sarvesh <30625354+Surreal-sarvesh@users.noreply.github.com>
2017-10-13 11:53:56 +0530

    aa
    aaa

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ var querytype = '0';
   }
   else if(req.body.data.investor == '0' && req.body.data.city !='0' && req.body.data.company == '0' && req.body.data.finflag != "0" && req.body.data.funding == '0') {
    // var query = "select company from transactions where city like '" + req.body.data.city + "%'";
-    var query = "select company from transactions where description like '%data%' and city like '" + req.body.data.city + "%'";
+    var query = "select company from transactions where description like '%" + req.body.data.finflag + "%' and city like '" + req.body.data.city + "%'";
     querytype = '4';
   }
   else if(req.body.data.investor == '0' && req.body.data.city =='0' && req.body.data.company != '0' && req.body.data.finflag == "0" && req.body.data.funding != '0') {
