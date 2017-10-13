de9aa148ebb533e86a78f9e41cb4475729234c56
Surreal-sarvesh <30625354+Surreal-sarvesh@users.noreply.github.com>
2017-10-13 15:39:04 +0530

    redeploys are coming
    dgdg

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(parser.urlencoded({
 /**
  * Database connection
  * TODO: remove the db credentials
- * from here later on.fhgfhgfghfgfghhsdjfhsdjsdhfsjfsdfasdadasdfsdfdsadjksdfdfjnky  sffs sdgdkfgsdssdf
+ * from here later on.fhgfhgfghfgfghhsdjfhsdjsdhfsjfsdfasdadasdfsdfdsadjksdfjnky  sffs sdgdkfgsdssdf
  */
 var connection = mysql.createConnection({
   host: 'us-cdbr-iron-east-05.cleardb.net',
